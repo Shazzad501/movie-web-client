fd2ff659bce8cbf637ec2a793b3198d61ae1e082
Shazzad501 <assazzad501@gmail.com>
2024-12-03 22:33:22 +0600

    error page creation done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <div>
-      error
+    <div className="flex flex-col gap-4 justify-center items-center py-28">
+      <h2 className="font-bold text-5xl">404</h2>
+      <div className="flex items-center justify-center">
+        <img src="https://i.ibb.co.com/MkmFJVc/error.webp" alt="" />
+      </div>
+      <h2 className="font-bold text-4xl">Page Not Found!!</h2>
+      <NavLink to='/' className='btn font-bold text-base bg-gray-500 text-white hover:text-black'>Go Back</NavLink>
     </div>
   );
 };
